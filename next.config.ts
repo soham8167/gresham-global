16285e6929ed2d4d95e44a3851679e041128041e
soham mondal <sohammondal50285@gmail.com>
2026-03-12 15:04:59 +0530

    add dynamic

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // Local Payload CMS (development)
       {
         protocol: "http",
         hostname: "localhost",
         port: "3001",
         pathname: "/media/**",
       },
+
+      // Render Payload CMS (production)
       {
         protocol: "https",
         hostname: "gresham-global-cms.onrender.com",
         pathname: "/media/**",
       },
     ],
+
     dangerouslyAllowLocalIP: true,
   },
 };
