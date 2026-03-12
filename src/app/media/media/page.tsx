1d38c4f54c4c7c03bdaa725435a9e587f741c354
soham mondal <sohammondal50285@gmail.com>
2026-03-12 16:26:36 +0530

    add dynamic

--- src/app/media/media/page.tsx
+++ src/app/media/media/page.tsx
@@ -396,14 +396,18 @@ export default function Page() {
       excerpt: item.excerpt,
       date: new Date(item.date).toDateString(),
 
-      mainImage: item.mainImage?.url
-  ? `${CMS_URL}${item.mainImage.url}`
-  : "",
+  //     mainImage: item.mainImage?.url
+  // ? `${CMS_URL}${item.mainImage.url}`
+  // : "",
 
-      publicationLogo: item.publicationLogo?.url
-  ? `${CMS_URL}${item.publicationLogo.url}`
-  : "",
+  //     publicationLogo: item.publicationLogo?.url
+  // ? `${CMS_URL}${item.publicationLogo.url}`
+  // : "",
 
+
+  // CORRECT - url already has the full path
+mainImage: item.mainImage?.url || "",
+publicationLogo: item.publicationLogo?.url || "",
       slug: item.slug,
     };
   }) || [];
